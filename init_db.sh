2910d13a736e7bf80d01a974c0d0def084bdb3b3
SilentWatch <burgesschen1990@gmail.com>
2025-01-09 10:00:45 -0500

    block num

--- init_db.sh
+++ init_db.sh
@@ -56,7 +56,7 @@ CREATE TABLE IF NOT EXISTS transactions (
     address VARCHAR(100),
     amount DOUBLE PRECISION,
     tx VARCHAR(100),
-    block_number INTEGER
+    block_number INTEGER,
     UNIQUE(timestamp, address, amount, tx, block_number)
 );
 
